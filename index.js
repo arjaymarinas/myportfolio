b29c8f4364c0b1a315244e19b3c21911234c4acc
Arjay <arjaymarinas@yahoo.com>
2024-02-11 14:54:17 +0800

    Initial commit with About and contact pages

--- index.js
+++ index.js
@@ -12,6 +12,18 @@ app.get("/", (req, res) => {
     res.render("index.ejs");
 });
 
+app.get("/contact", (req, res) => {
+    res.render("contact.ejs");
+});
+
+app.get("/about", (req, res) => {
+    res.render("about.ejs");
+});
+
+app.get("/myprocess", (req, res) => {
+    res.render("myprocess.ejs");
+});
+
 app.listen(port, ()=>{
     console.log(`Server is runnin on port ${port}`);
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,5 +10,10 @@ export default {
   plugins: [
    
   ],
+  variants: {
+    extend: {
+      gridTemplateColumns: ['responsive', 'hover', 'focus'],
+    },
+  },
 }
 
